fe8ed5fb7bba3e2a3b0e4e2586f77528e7cb956d
Timothée ZERBIB <tim.zerb@yahoo.fr>
2025-09-25 13:43:30 -0400

    Fix extension.h

--- extensions.h
+++ extensions.h
@@ -2,6 +2,6 @@ enum extstate { UNUSED, LOADED, ATTACHED };
 
 struct extension {
   enum extstate state;         // extension state
-  void* text;                  // Pointer to the extension code
-  char name[16]                // extension name (debugging)
+  void* (*text)(void);         // Pointer to the extension code
+  char name[16];               // extension name (debugging)
 };
