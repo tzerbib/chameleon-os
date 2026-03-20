f93fd2263a51ccf0d2ebb880cce50579369a2cbb
Ting He <ting_he1@brown.edu>
2026-03-19 21:18:57 -0400

    stack init; ns init placeholder

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "ethernet_vlan.h"
 #include "types.h"
 #include "defs.h"
 #include "param.h"
@@ -32,6 +33,8 @@ main(void)
   fileinit();      // file table
   ideinit();       // disk 
   pciinit();       // pci devices
+  vlanstackinit(); // vlan stack
+  globalnsinit();  // global namespace init (dummy rn)
   netinit();       // networking
   startothers();   // start other processors
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
