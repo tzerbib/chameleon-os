4531b5691130b5681e4be97241fad5d640bd0fff
Julie Wang <juliewang617@gmail.com>
2026-04-02 13:53:58 -0400

    remove debugging prints

--- x4.ext.c
+++ x4.ext.c
@@ -5,27 +5,18 @@ char* INNER_MAP_NAME = "syscall_counts";
 // Example read/write extension at mkdir hookpoint
 // trying to do something like count how many times mkdir is called
 int EXT_ENTRY_RO(mkdir, char const* path){
-  xputs("1\n");
   struct hashmap *maps = my_maps(); 
-  xputs("2 ");
-  xputx((int)maps); 
-  xputs("\n");
   
   struct hashmap *syscall_counts = (struct hashmap*)hm_get(maps, INNER_MAP_NAME);
-  xputs("3\n");
   if (syscall_counts == 0) {
-    xputs("3.5\n");
     syscall_counts = hm_alloc();
-    xputs("4\n");
     if (syscall_counts == 0){
       return 0;
     }
     hm_put(maps, INNER_MAP_NAME, syscall_counts);
-    xputs("5\n");
   }
 
   int count = (int)hm_get(syscall_counts, "mkdir"); 
-  xputs("6\n");
 
   hm_put(syscall_counts, "mkdir", (void*)(count+1));
 
