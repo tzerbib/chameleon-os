68d18877f4d40c4296cfc7cc58ccf30446991dd9
Julie Wang <juliewang617@gmail.com>
2026-04-16 11:46:17 -0400

    add itoa

--- ulib.c
+++ ulib.c
@@ -92,6 +92,25 @@ atoi(const char *s)
   return n;
 }
 
+int 
+itoa(int n, char s[]){
+  int i, sign;
+
+  if ((sign = n) < 0)  
+      n = -n;          
+  
+  i = 0;
+  do {      
+      s[i++] = n % 10 + '0';  
+  } while ((n /= 10) > 0);   
+  
+  if (sign < 0)
+      s[i++] = '-';
+  
+  s[i] = '\0';
+  reverse(s); 
+}
+
 void*
 memmove(void *vdst, const void *vsrc, int n)
 {
